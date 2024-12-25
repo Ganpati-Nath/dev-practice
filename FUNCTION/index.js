0a176408eddc45497f1a31f9c35fbd0c08d61c96
Ganpati <ganpatinath07@gmail.com>
2024-12-25 12:53:54 +0530

    dev...

--- FUNCTION/index.js
+++ FUNCTION/index.js
@@ -5,3 +5,8 @@ function printMyName(str1, str2) {
 let ans = printMyName("Ganpati", "Nath");
 console.log(ans);
 console.log("Done...");
+
+let getMultiple = function(a, b) {
+    return a*b;
+}
+console.log(getMultiple(5, 3));
