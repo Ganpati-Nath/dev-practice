c4765bf0037005b8691ee709773edc4d6cce835f
Ganpati <ganpatinath07@gmail.com>
2024-12-26 14:25:06 +0530

    array fundamentals

--- ARRAYS/index.js
+++ ARRAYS/index.js
@@ -1,34 +1,62 @@
-let obj = {
-    name : "Name",
-    age : 25,
-    weight : "85 kg",
-    height : "6 ft 1 inch",
-    greet : () => {
-        console.log("Done");
-    }
-};
-console.log(obj);
-obj.greet();
+// let obj = {
+//     name : "Name",
+//     age : 25,
+//     weight : "85 kg",
+//     height : "6 ft 1 inch",
+//     greet : () => {
+//         console.log("Done");
+//     }
+// };
+// console.log(obj);
+// obj.greet();
 
-console.log(typeof(obj));
-let ob = obj;
+// console.log(typeof(obj));
+// let ob = obj;
 
-let arr = [1, 2, false, "Name"];
-console.log(arr);
-console.log(typeof(arr));
+// let arr = [1, 2, false, "Name"];
+// console.log(arr);
+// console.log(typeof(arr));
 
-let brr = new Array("Name", 1, false);
-brr.push("Done");
-brr.pop();
-brr.shift();
-brr.unshift("Name");
-brr.push(20);
-brr.push(40);
-brr.push(60);
-console.log(brr.slice(2, 4));
-brr.splice(1, 0, "Done");
-console.log(brr);
-console.log(typeof(brr));
+// let brr = new Array("Name", 1, false);
+// brr.push("Done");
+// brr.pop();
+// brr.shift();
+// brr.unshift("Name");
+// brr.push(20);
+// brr.push(40);
+// brr.push(60);
+// console.log(brr.slice(2, 4));
+// brr.splice(1, 0, "Done");
+// console.log(brr);
+// console.log(typeof(brr));
 
-brr[0] = "firstName lastName";
-console.log(brr[0]);
+// brr[0] = "firstName lastName";
+// console.log(brr[0]);
+
+let arr = [10, 20, 30, 11, 51, 33];
+
+// let ans = arr.map((number) => {
+//     return number*number;
+// })
+
+// let ans = arr.filter((num) => {
+//     if(num % 2 !== 0) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// });
+
+// let ans = arr.reduce((acc, curr) => {
+//     return acc + curr;
+// }, 0);
+
+// let ans = arr.sort((a, b) => b - a);
+
+arr.forEach((val, idx) => {
+    console.log("Number : ", val, " Index : ", idx);
+})
+
+// console.log(ans);
+// console.log(typeof(ans));
